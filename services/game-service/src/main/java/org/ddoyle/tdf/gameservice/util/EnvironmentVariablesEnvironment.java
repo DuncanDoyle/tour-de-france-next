1f5e81892b4800039e2b9482edcccdae4096ce29
Duncan Doyle <Duncan.Doyle@gmail.com>
2017-06-10 10:44:00 +0200

    Added additional logging to show which environment gets loaded.

--- services/game-service/src/main/java/org/ddoyle/tdf/gameservice/util/EnvironmentVariablesEnvironment.java
+++ services/game-service/src/main/java/org/ddoyle/tdf/gameservice/util/EnvironmentVariablesEnvironment.java
@@ -1,11 +1,19 @@
 package org.ddoyle.tdf.gameservice.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class EnvironmentVariablesEnvironment implements Environment {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(EnvironmentVariablesEnvironment.class);
+	
 	private static final String PLAYER_SERVICE_URL_ENV_VARIABLE_NAME = "PLAYER_SERVICE_URL";
 	
 	private static final String PLAYER_SERVICE_URL = System.getenv(PLAYER_SERVICE_URL_ENV_VARIABLE_NAME);
-	
+
+	public EnvironmentVariablesEnvironment() {
+		LOGGER.info("Loading EnvironmentVariables Environment.");
+	} 
 	
 	@Override
 	public String getPlayerServiceUrl() {
--- services/game-service/src/main/java/org/ddoyle/tdf/gameservice/util/SystemPropertiesEnvironment.java
+++ services/game-service/src/main/java/org/ddoyle/tdf/gameservice/util/SystemPropertiesEnvironment.java
@@ -1,17 +1,27 @@
 package org.ddoyle.tdf.gameservice.util;
 
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class SystemPropertiesEnvironment implements Environment {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(SystemPropertiesEnvironment.class);
+	
 	private static final String PLAYER_SERVICE_URL_SYSTEM_PROPERT_NAME = "player.service.url";
 	
 	private static final String PLAYER_SERVICE_URL = System.getProperty(PLAYER_SERVICE_URL_SYSTEM_PROPERT_NAME);
-
+	
+	public SystemPropertiesEnvironment() {
+		LOGGER.info("Loading SystemProperties Environment.");
+	}
+	
+	
 	@Override
 	public String getPlayerServiceUrl() {
 		return PLAYER_SERVICE_URL;
 	}
-	
-	
 	
 	
 }
